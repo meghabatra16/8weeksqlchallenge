9c7d26db15f18cbe89c30a9057148f98d6fb2550
Megha Batra <81023802+meghabatra16@users.noreply.github.com>
2021-07-29 13:14:43 +0530

    Update queries.sql

--- Case Study 1/queries.sql
+++ Case Study 1/queries.sql
@@ -1,7 +1,7 @@
 /* --------------------
    Case Study Questions
    --------------------*/
-Query 1
+##Query 1
 
 -- 1. What is the total amount each customer spent at the restaurant?
 select customer_id, sum(price) 
@@ -17,7 +17,9 @@ order by customer_id;
 | B           | 74  |
 | C           | 36  |
 
-Query 2
+--------
+
+#Query 2
 
 -- 2. How many days has each customer visited the restaurant?
 select customer_id, count(distinct order_date) 
@@ -30,7 +32,9 @@ group by customer_id;
 | B           | 6     |
 | C           | 2     |
 
-Query 3
+--------
+
+##Query 3
 
 --3. What was the first item from the menu purchased by each customer?
 select customer_id, m.product_id, m.product_name, order_date
@@ -40,14 +44,16 @@ on m.product_id = s.product_id
 where order_date = '2021-01-01'
 order by customer_id;
  
-|customer_id   | product_id |product_name  |   order_date  |
+|customer_id   | product_id |product_name  |   order_date  | 
 | A	         |     1	    |   "sushi"	 |  "2021-01-01" |
 | A	         |     2	    |   "curry"	 |  "2021-01-01" |
 | B	         |     2	    |   "curry"	 |  "2021-01-01" |
 | C	         |     3	    |   "ramen"	 |  "2021-01-01" |
-| C	         |     3	    |    "ramen"   |  "2021-01-01" |
+| C	         |     3	    |    "ramen" |  "2021-01-01" |
 
-Query 4
+--------
+
+#Query 4
 
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
 
@@ -62,8 +68,10 @@ limit 1;
 | ------------ | ----- |
 | ramen        | 8     |
 
+--------
 
-Query 5
+##Query 5
+
 -- 5. Which item was the most popular for each customer?
 with ctediners as 
 	(select s.customer_id, s.product_id, count(*) as num,
@@ -87,8 +95,7 @@ limit = 5
 | C           | ramen        |
 
 
--------
-
+---------
 
 ##Query 6
 -- 6. Which item was purchased first by the customer after they became a member?
@@ -104,11 +111,13 @@ inner join members as me
 on d.customer_id = me.customer_id
 where order_date >= join_date AND product_id < 3
 
-|customer_id   | product_id |rn      |   order_date  |
-| A	         |     2	    |  2     |  "2021-01-07" |
-| B	         |     1	    |   4	 |  "2021-01-11" |
+|customer_id     | product_id       | rn     	 | order_date   |
+| A	         |     2	    |   2      	 | "2021-01-07  |  
+| B	         |     1	    |   4	 | "2021-01-11" |
 
-Query 7
+--------
+
+##Query 7
 -- 7. Which item was purchased just before the customer became a member?
 
 With ctediners AS (
@@ -123,9 +132,12 @@ on d.customer_id = me.customer_id
 where order_date < join_date and rn = 4
 
 |customer_id   | product_id |  rn    |   order_date  |
-| A	         |     2	    |  4     |  "2021-01-01" |
-| A	         |     1	    |  4	    |  "2021-01-01" |
+| A	       |     2	    |  4     |  "2021-01-01" |
+| A	       |     1	    |  4     |  "2021-01-01" |
 | B            |     1      |  4     |  "2021-01-04" |
 
-Query 8
+--------
+
+##Query 8
+
 8. What is the total items and amount spent for each member before they became a member?
