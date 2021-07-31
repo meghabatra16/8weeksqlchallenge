154c6f541d5177fef926037dee39e4945b8c68ab
Megha Batra <81023802+meghabatra16@users.noreply.github.com>
2021-07-31 16:56:35 +0530

    Update pizza_metrics.sql

--- Case Study #2/pizza_metrics.sql
+++ Case Study #2/pizza_metrics.sql
@@ -1,1 +1,42 @@
+##Query 1
+--How many pizzas were ordered?
+select count(*) as Totalpizzas 
+from customer_orders;
+
+| Totalpizzas          |
+| -------------------- |
+| 14                   |
+
+------
+
+##Query 2
+--How many unique customer orders were made?
+select count(distinct order_time) as unique_orders
+from customer_orders;
+
+| unique_orders        |
+| -------------------- |
+| 10                   |
+
+-------
+
+##Query 3
+--How many successful orders were delivered by each runner?
+select runner_id, count(*)
+from runner_orders
+where cancellation is null
+group by  runner_id;
+
+| runner_id | count |
+| --------- | ----- |
+| 1         | 4     |
+| 2         | 3     |
+| 3         | 1     |
+
+------
+
+##Query 4
+How many of each type of pizza was delivered?
+
+
 
