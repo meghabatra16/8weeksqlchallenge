0a4b06f8f598ebc7da35ac9397ce541aad82a34c
Megha Batra <81023802+meghabatra16@users.noreply.github.com>
2021-08-04 13:31:08 +0530

    Update Runner_customer_experience.sql

--- Case Study #2/Runner_customer_experience.sql
+++ Case Study #2/Runner_customer_experience.sql
@@ -2,3 +2,21 @@
 
 #Query 1
 --How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
+select to_char(registration_date, 'Week') as W, count(*)
+from runners
+group by W
+
+|    W    | count|
+|----------------|
+|1st week |  2   |
+|2nd week |  1   |
+|3rd week |  1   |
+
+-------
+
+#Query 2
+--What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
+
+
+
+
