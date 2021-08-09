c38ff24c3a75635aacc84a5445f372495fe07d94
Megha Batra <81023802+meghabatra16@users.noreply.github.com>
2021-08-09 23:46:21 +0530

    Update Runner_customer_experience.sql

--- Case Study #2/Runner_customer_experience.sql
+++ Case Study #2/Runner_customer_experience.sql
@@ -16,13 +16,51 @@ group by W
 
 #Query 2
 --What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
+With ctediners as (
+select cu.order_time, r.pickup_time, r.runner_id
+from runner_orders as r
+inner join customer_orders as cu
+on cu.order_id = r.order_id
+)
+select cte.runner_id, extract(minute from avg(cte.pickup_time - cte.order_time)) as avg_time
+from ctediners as cte
+where cte.pickup_time is not null
+group by cte.runner_id
+order by cte.runner_id
+
+
+| runner_id | avg_time     |
+----------------------------
+| 1         | 15           |
+| 2         | 23           |
+| 3         | 10           |
+
+--------
+
+#Query 3
+--Is there any relationship between the number of pizzas and how long the order takes to prepare?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 #Query 4
 --What was the average distance travelled for each customer?
-with ctediners as (
+With ctediners as (
 select cu.customer_id, r.pickup_time, r.distance
 from runner_orders as r
 inner join customer_orders as cu
@@ -33,6 +71,7 @@ from ctediners as cte
 group by cte.customer_id
 order by cte.customer_id
 
+
 | customer_id | round |
 -----------------------
 | 101         | 20.00 |
@@ -41,3 +80,39 @@ order by cte.customer_id
 | 104         | 10.00 |
 | 105         | 25.00 |
 
+--------
+
+#Query 5
+--What was the difference between the longest and shortest delivery times for all orders?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Query 6
+--What was the average speed for each runner for each delivery and do you notice any trend for these values?
+select runner_id, order_id, round(avg(cast(distance as decimal)/cast(duration as decimal)),2) as speed
+from runner_orders
+where cancellation is null
+group by runner_id, order_id
+order by runner_id, order_id
+
+| runner_id | order_id | speed  |
+---------------------------------
+| 1         | 1        | 0.63   |
+| 1         | 2        | 0.74   |
+| 1         | 3        | 0.67   |
+| 1         | 10       | 1.00   |
+| 2         | 4        | 0.59   |
+| 2         | 7        | 1.00   |
+| 2         | 8        | 1.56   |
+| 3         | 5        | 0.67   |
