b68a4944ef66bd4fe0bd82aeed4fadfcc30a3836
Megha Batra <81023802+meghabatra16@users.noreply.github.com>
2021-08-15 00:07:36 +0530

    Update cleantables.sql

--- Case Study #2/cleantables.sql
+++ Case Study #2/cleantables.sql
@@ -35,24 +35,49 @@ from customer_orders
 | 10       | 104         | 1        |            |        | 2020-01-11T18:34:49.000Z |
 | 10       | 104         | 1        | 2, 6       | 1, 4   | 2020-01-11T18:34:49.000Z |
 
+
 ------------
+
 
 --Cleaning Table runner_orders
 
-create temp table temprunner_orders as 
+drop table if exists temprunner_orders;
+
+create temp table temprunner_orders as (
 select *,
-case when cancellation = '' then NULL
-     when cancellation = 'null' then NULL
+case 
+	 when cancellation like '' then NULL
+     when cancellation like 'null' then NULL
 	 else cancellation
-end as cancellations,
-case when pickup_time = 'null' then NULL
+     end as cancellations,
+case 
+	 when pickup_time like 'null' then NULL
 	 else pickup_time
-end as pickup_time
-case when distance = 'null' then NULL
-     when distance = '%km' then
-case when duration = 'null' then NULL
-     when duration = '%minutes' then 
-	 
-from runner_orders
+     end as pickup,
+case 
+	 when distance like 'null' then NULL
+     when distance like '%km' then trim(trailing 'km' from distance)
+	 else distance
+     end as distance_km,
+case 
+	 when duration like 'null' then NULL
+     when duration like '%minutes' then trim( trailing 'minutes' from duration)
+	 else duration 
+     end as duration_minutes
+from runner_orders);
+
+| order_id | runner_id | pickup_time         | distance | duration | cancellation            |
+----------------------------------------------------------------------------------------------
+| 1        | 1         | 2020-01-01 18:15:34 | 20       | 32       |                         |
+| 2        | 1         | 2020-01-01 19:10:54 | 20       | 27       |                         |
+| 3        | 1         | 2020-01-03 00:12:37 | 13.4     | 20       |                         |
+| 4        | 2         | 2020-01-04 13:53:03 | 23.4     | 40       |                         |
+| 5        | 3         | 2020-01-08 21:10:57 | 10       | 15       |                         |
+| 6        | 3         |                     |          |          | Restaurant Cancellation |
+| 7        | 2         | 2020-01-08 21:30:45 | 25       | 25       |                         |
+| 8        | 2         | 2020-01-10 00:15:02 | 23.4     | 15       |                         |
+| 9        | 2         |                     |          |          | Customer Cancellation   |
+| 10       | 1         | 2020-01-11 18:50:20 | 10       | 10       |                         |
 
 
+-----------
