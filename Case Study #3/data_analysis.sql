fba15fd7c402d13e0d5397bedeb015e5635ea251
Megha Batra <81023802+meghabatra16@users.noreply.github.com>
2021-08-20 13:03:38 +0530

    Update data_analysis.sql

--- Case Study #3/data_analysis.sql
+++ Case Study #3/data_analysis.sql
@@ -1,1 +1,25 @@
+/* Data Analysis */
+
+--1.How many customers has Foodie-Fi ever had?
+select count(distinct customer_id) as total
+from foodiee
+
+| total |
+---------
+| 1000  |
+
+-------
+
+--2.What is the monthly distribution of trial plan start_date values for our dataset - use the start of the month as the group by value
+
+
+
+
+
+
+
+
+
+
+
 
