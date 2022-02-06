b4db18b1930b002ad14def201cce4c3f64f68f62
Megha Batra <81023802+meghabatra16@users.noreply.github.com>
2022-02-06 02:20:54 -0500

    Update Customer_Node_Exploration.sql

--- Case Study #4/Customer_Node_Exploration.sql
+++ Case Study #4/Customer_Node_Exploration.sql
@@ -1,2 +1,13 @@
-#1. How many unique nodes are there on the Data Bank system?
+/* Case Study - 4 */
+
+##1. How many unique nodes are there on the Data Bank system?
 select count(distinct(node_id)) from customer_nodes
+
+##2 What is the number of nodes per region?
+select region_id, count(*) from customer_nodes
+group by region_id
+order by region_id
+
+or
+
+
