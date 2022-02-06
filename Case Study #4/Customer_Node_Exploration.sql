ef17163da99000b5599c33d4b7ceacee20e880df
Megha Batra <81023802+meghabatra16@users.noreply.github.com>
2022-02-06 14:31:43 -0500

    Update Customer_Node_Exploration.sql

--- Case Study #4/Customer_Node_Exploration.sql
+++ Case Study #4/Customer_Node_Exploration.sql
@@ -3,11 +3,19 @@
 ##1. How many unique nodes are there on the Data Bank system?
 select count(distinct(node_id)) from customer_nodes
 
+
+
 ##2 What is the number of nodes per region?
-select region_id, count(*) from customer_nodes
+select region_id, count(distinct node_id) from customer_nodes
 group by region_id
 order by region_id
 
-or
+
+##3 How many customers are allocated to each region?
+select region_id, count(distinct customer_id) as customers
+from customer_nodes
+group by region_id
+
+##4 How many days on average are customers reallocated to a different node?
 
 
